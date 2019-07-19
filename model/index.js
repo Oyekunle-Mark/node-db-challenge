93aaa03dba8e46d10ff591fee9d5c6547e5b1e52
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-19 09:34:08 +0100

    Added the functionality for endpoint to return newly created project

--- model/index.js
+++ model/index.js
@@ -1,6 +1,16 @@
 const db = require('../data/dbConfig');
 
-const addProjects = project => db('projects').insert(project);
+const get = id => {
+  const query = db('projects');
+
+  return id ? query.where({ id }) : query;
+};
+
+const addProjects = async project => {
+  const [id] = await db('projects').insert(project);
+
+  return get(id);
+};
 
 module.exports = {
   addProjects,
