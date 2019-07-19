b6cb982d3b91cd95c3970370a1b5630720a23a39
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-19 13:41:00 +0100

    Added a final catch step for wrong url ;

--- server.js
+++ server.js
@@ -18,4 +18,11 @@ server.get('/', (req, res) =>
 server.use('/api/projects', projectRouter);
 server.use('/api/actions', actionRouter);
 
+server.use((req, res) =>
+  res.status(404).json({
+    status: 404,
+    message: 'Take a good look at the URL.',
+  }),
+);
+
 module.exports = server;
